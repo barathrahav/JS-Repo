aa9f8b0a7e9ac7beaa38ee3a9698773cb4356fcb
barathrahav <barathrahav@gmail.com>
2023-05-04 12:49:22 +0530

    All Tasks added

---
+++ Task - 3/task3.js
@@ -0,0 +1,5 @@
+var yearOfbirth=1994;
+var yearOffuture=2025;
+var age=(yearOffuture-yearOfbirth);
+var agePossibility=(age+1);
+console.log("I will be either " +age+ " or " +agePossibility+ " in " +yearOffuture)
---
+++ Task - 4/task4.js
@@ -0,0 +1,2 @@
+var currentAge=29, futureAge=60, dailyExpense=500, totalExpense=((futureAge-currentAge)*(dailyExpense*365));
+console.log("You will need " +totalExpense+ " to last you until the ripe old age of " +futureAge)
