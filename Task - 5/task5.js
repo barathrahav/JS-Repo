684b21160385dbb0e5ec9ddd1a6e6b26c45b6a5c
barathrahav <barathrahav@gmail.com>
2023-05-04 16:35:57 +0530

    Tasks added.

---
+++ Task - 5/task5.js
@@ -0,0 +1,5 @@
+var a=5, b=7, c=13, d=7, x=10, y=10;
+alert(a*b);
+alert(13%7);
+console.log(x+y);
+console.log(x+=20);
---
+++ Task - 6/task6.js
@@ -0,0 +1,6 @@
+var anchoviesPizzaScore = 0, pepperoniPizzaScore = 4, hawaiianPizzaScore = 4, chickenBaconRanchPizzaScore = 6;
+console.log(anchoviesPizzaScore < pepperoniPizzaScore);
+console.log(chickenBaconRanchPizzaScore > hawaiianPizzaScore);
+console.log(hawaiianPizzaScore > pepperoniPizzaScore);
+console.log(pepperoniPizzaScore <= chickenBaconRanchPizzaScore);
+console.log(hawaiianPizzaScore >= pepperoniPizzaScore);
